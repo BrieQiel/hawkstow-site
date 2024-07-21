25d664a010ea7d2f9d40a1b96e161c2b237584c2
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-07-21 09:31:39 +0800

    services

---
+++ components/Services.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container class="">
+    <h1 class="font-Lexend font-semibold text-[#175500] text-3xl mb-2">
+      Our Services
+    </h1>
+    <v-divider
+      class="mx-4 mb-8 border-opacity-100 border-[#d4bf18]"
+      :thickness="5"
+    ></v-divider>
+
+    <v-row class="h-100 px-8 flex flex-row" align="center">
+      <v-col>
+        <!-- image to show on mobile view -->
+        <v-img :src="`/images/${image}`" class="hidden-md-and-up mb-4" />
+
+        <h1
+          class="uppercase text-[#175500] text-lg md:text-xl font-semibold mb-2"
+        >
+          {{ title }}
+        </h1>
+        <p class="text-sm md:text-base">{{ description }}</p>
+      </v-col>
+
+      <!-- image to show on web view  -->
+      <v-col class="hidden-sm-and-down">
+        <v-img :src="`/images/${image}`" class="">
+          <v-container
+            class="h-full font-Lexend text-lg text-white bg-[#84bf6e] opacity-5 hover:opacity-100"
+            justify="center"
+            align="center"
+          >
+            <p class="font-bold">{{ imageName }}</p>
+            <br />
+            <p class="font-bold">Area</p>
+            <p class="font-light">{{ area }} sqm</p>
+            <p class="font-bold">Location</p>
+            <p class="font-light">{{ location }}</p>
+          </v-container>
+        </v-img>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  description: String,
+  image: String,
+  imageName: String,
+  area: String,
+  location: String,
+});
+</script>
---
+++ data/servicesdata.ts
@@ -0,0 +1,38 @@
+export const servicesData = [
+  {
+    title: "Performance Efficiency",
+    description:
+      "Hawkstow is steadfast to the advancement of a principle anchored on Efficiency Plus Accomplishment. The company sets expansive targets in every endeavor it undertakes through the essentials of Efficiency Plus Accomplishment – Improvement and Modernization, Client Focus, Discipline and Human Resource Development -  to attain its vision, to be the company of primary choice for all our future and present clients, capable of adopting to change and improving our humble practices in our works and contracts.",
+    image: "placeholder.jpg",
+    imageName: 'ROADDDDD',
+    area: '662',
+    location: 'Manila',
+  },
+  {
+    title: "Improvement and Modernization",
+    description:
+      "Our company is zealous about continuously modernizing our construction methods and techniques to meet the fast changing complexity of demands in the construction industry, and, Investing in research and human resource development through collaboration with our stakeholders and other educational institutions.",
+    image: "placeholder.jpg",
+    imageName: 'Govt Building',
+    area: '687',
+    location: 'Manila',
+  },
+  {
+    title: "Client Focus",
+    description:
+      "Service and fully understanding our customers’ needs are cultures our company value most. Collectively working with them in every stage of the project, advances bespoke meetings and collaborative modifications such as work variations, are proficiently decided.",
+    image: "placeholder.jpg",
+    imageName: 'YEaa',
+    area: '387',
+    location: 'Manila',
+  },
+  {
+    title: "Discipline and Human Resource Development",
+    description:
+      "Our employees are our company’s greatest asset.  Continuous education on traditional and modern construction methods and techniques and practical drills specially on Construction Occupational Health and Safety are part of the fundamentals in our regular job site activities. We responsibly collaborate with our stakeholders, clients and the communities in which we operate, to safeguard from and management of environmental impacts triggered and effected by our activities.",
+    image: "placeholder.jpg",
+    imageName: 'Location',
+    area: '999',
+    location: 'Manila',
+  },
+];
