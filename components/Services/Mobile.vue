3d6b0c23e042991efd9c2ec4a03e652a331b39da
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-07-29 01:00:19 +0800

    created new Services components

---
+++ components/Services/Mobile.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <v-img :src="`/images/${image}`" />
+        <div class="mx-5 mt-5">
+            <h1
+              class="uppercase text-[#175500] text-lg md:text-xl leading-snug font-semibold mb-2"
+            >
+              {{ title }}
+            </h1>
+            <p class="text-xs md:text-base">{{ description }}</p>
+          </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  description: String,
+  image: String,
+  imageName: String,
+  area: String,
+  location: String,
+});
+</script>
---
+++ components/Services/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-9/12 md:w-10/12 mx-auto">
+    <HeadingTitle :title="head" />
+
+    <ServicesMobile
+      :title="title"
+      :description="description"
+      :image="image"
+      :imageName="imageName"
+      :area="area"
+      :location="location"
+      class="hidden-md-and-up mb-4"
+    />
+
+    <ServicesDesktop
+      :title="title"
+      :description="description"
+      :image="image"
+      :imageName="imageName"
+      :area="area"
+      :location="location"
+      class="hidden-sm-and-down"
+    />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: String,
+  description: String,
+  image: String,
+  imageName: String,
+  area: String,
+  location: String,
+});
+
+const head = "Our Services";
+</script>
+
+<!--
+-->
