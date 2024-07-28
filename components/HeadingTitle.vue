5e1123bcf65a5f542495aa250aa88d0ebe4dac96
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-07-29 00:59:46 +0800

    created component for heading title

---
+++ components/HeadingTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="font-Lexend font-semibold text-[#175500] text-3xl mb-2 mt-5">
+        {{ props.title }}
+      </h1>
+      <v-divider
+        class="mx-4 mb-8 border-opacity-100 border-[#d4bf18]"
+        :thickness="5"
+      ></v-divider>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+})
+</script>
