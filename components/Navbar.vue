b1c80abcc700f6f2513288f1433bb9446909f702
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-07-31 21:39:28 +0800

    changed assigned numbers for new slides

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
   <v-toolbar height="75" color="white">
     <v-row class="mx-4">
-      <v-img src="public/images/logo.webp" max-width="50"></v-img>
+      <v-img src="public/images/logo.webp" max-width="50" @click="toSlide(0)" class="cursor-pointer"></v-img>
       <div class="flex">
         <v-toolbar-title
           class="cursor-pointer ml-4"
-          @click="emitToSlide(0)"
+          @click="toSlide(0)"
         >
           <span class="text-[#34a90c] font-extrabold">HAWK</span>
           <span class="text-[#d4bf18] font-extrabold">STOW</span>
@@ -16,22 +16,18 @@
       </div>
       <v-spacer></v-spacer>
       <v-toolbar-items class="flex hidden-sm-and-down">
-        <v-btn variant="text" @click="emitToSlide(1)">About</v-btn>
-        <v-btn variant="text" @click="emitToSlide(5)">Projects</v-btn>
-        <v-btn variant="text" @click="emitToSlide(6)">Contact</v-btn>
+        <v-btn variant="text" @click="toSlide(1)">About</v-btn>
+        <v-btn variant="text" @click="toSlide(6)">Projects</v-btn>
+        <v-btn variant="text" @click="toSlide(7)">Contact</v-btn>
       </v-toolbar-items>
     </v-row>
   </v-toolbar>
 </template>
 
 <script setup>
-import { ref } from "vue";
-
-const currentSlide = ref(0);
-
 const emit = defineEmits(["toSlide"]);
 
-function emitToSlide(slide) {
-  emit("emitToSlide", slide);
+function toSlide(slide) {
+  emit("toSlide", slide);
 }
 </script>
