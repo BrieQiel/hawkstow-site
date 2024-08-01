ab3afa4a485c17045525ad153386d167f4aa2fa6
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-01 16:17:25 +0800

    added service card to display the list of services

---
+++ components/ServicesCard/Desktop.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card
+    v-for="services in servicesList"
+    flat
+    class="h-64 w-48 m-4 hidden-sm-and-down"
+  >
+    <v-img
+      :src="`/images/services/${services.icon}`"
+      :width="100"
+      aspect-ratio="1"
+      lazy-src="assets/images/bg.png"
+      cover
+      class="mt-4 mx-auto"
+    />
+    <p class="mt-4 mx-4 text-center">{{ services.title }}</p>
+  </v-card>
+</template>
+
+<script setup>
+const props = defineProps({
+  servicesList: Array,
+});
+</script>
---
+++ components/ServicesCard/Mobile.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-carousel
+    :show-arrows="false"
+    cycle
+    touch
+    hide-delimiter-background
+    color="#d4bf18"
+    class="mx-8 h-4 hidden-md-and-up"
+    height="280"
+  >
+    <v-carousel-item v-for="services in servicesList">
+      <v-card flat class="h-fit w-56 m-4 mx-auto">
+        <v-img
+          :src="`/images/services/${services.icon}`"
+          :width="100"
+          height="100"
+          lazy-src="assets/images/bg.png"
+          cover
+          class="mt-4 mx-auto"
+        ></v-img>
+        <p class="mt-4 mx-4 text-center">{{ services.title }}</p>
+      </v-card>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script setup>
+const props = defineProps({
+  servicesList: Array,
+});
+</script>
