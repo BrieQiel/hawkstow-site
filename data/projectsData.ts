5b35051bca5e9334515fb1d82a5ce0945ce3f11c
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-01 20:25:58 +0800

    removed added "animation" key

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,26 +2,21 @@ export const projectsData = [
   {
     name: "Department of Public Works and Highways",
     logo: "dpwh.png",
-    animation: "client1",
   },
   {
     name: "Department of Agrarian Reform",
     logo: "dar.png",
-    animation: "client2",
   },
   {
     name: "Department of Education",
     logo: "deped.png",
-    animation: "client3",
   },
   {
     name: "Department of Health",
     logo: "doh.png",
-    animation: "client4",
   },
   {
     name: "Department of Transportation",
     logo: "dotr.png",
-    animation: "client5",
   },
 ];
