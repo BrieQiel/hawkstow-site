3089492a92a45e4887a6f4b4aa10ca3d046e6fcd
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-01 21:01:43 +0800

    sample desktop projects

--- components/Projects/Desktop.vue
+++ components/Projects/Desktop.vue
@@ -1,6 +1,34 @@
 <template>
-  <div class="h-100 mx-20 mt-20 text-left" align="center">
+  <div class="h-100 mx-20 mt-12 text-center" align="center">
+    <div class="flex flex-row mb-16">
+      <v-img
+      src="assets/images/bg.png"
+      :width="200"
+      aspect-ratio="3/4"
+      lazy-src="assets/images/bg.png"
+      cover
+      class="mx-8"
+    />
+    <v-img
+      src="assets/images/bg.png"
+      :width="200"
+      aspect-ratio="3/4"
+      lazy-src="assets/images/bg.png"
+      cover
+      class="mx-8"
+    />
+    <v-img
+      src="assets/images/bg.png"
+      :width="200"
+      aspect-ratio="3/4"
+      lazy-src="assets/images/bg.png"
+      cover
+      class="mx-8"
+    />
+    </div>
+
     <Marquee :clients="clients" />
+    <p class="font-Lexend font-semibold text-[#175500] text-xl">&#38; <br /> PRIVATE BUSINESSES</p>
   </div>
 </template>
 
