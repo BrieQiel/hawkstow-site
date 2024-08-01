ece00fb500a929ebd2f49453763739e1872e4e3d
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-01 16:08:40 +0800

    removed about paragraph in homepage and placed it in about slide

--- components/AboutSlide.vue
+++ components/AboutSlide.vue
@@ -1,3 +1,30 @@
 <template>
-    about
+  <div class="w-9/12 md:w-10/12 mx-auto mt-12">
+    <HeadingTitle :title="title" />
+
+    <p class="text-sm md:text-base mx-8 mt-12">
+      <span class="font-bold">Hawkstow</span>
+      <span class="">
+        is a privately owned multidisciplinary engineering enterprise with
+        employed dedicated professionals having extensive and proud experiences
+        in different construction services, assisted by progressive and quixotic
+        young people specializing in different fields of the construction
+        industry.
+      </span>
+    </p>
+
+    <p class="font-Lexend text-base md:text-lg mx-8 mt-8 text-center">
+      Our Services include:
+    </p>
+    <v-row class="flex mt-4 justify-center">
+      <ServicesCard :servicesList="servicesList" />
+    </v-row>
+  </div>
 </template>
+
+<script setup>
+import { servicesData2 } from "/data/servicesdata.ts";
+
+const servicesList = ref(servicesData2);
+const title = "About Us";
+</script>
--- components/HomepageSlide.vue
+++ components/HomepageSlide.vue
@@ -1,23 +1,23 @@
 <template>
-  <v-img class="h-screen" src="public/images/bg.png" cover>
-    <v-container class="text-center h-3/4 content-center pb-64">
+    <v-container class="text-center h-3/4 pb-64 bg-transparent">
       <h1
-        class="font-Lexend font-semibold leading-relaxed md:leading-relaxed text-white underline decoration-[#d4bf18] underline-offset-8 mb-24 text-4xl md:text-5xl"
+        class="fadefromleft font-Lexend font-semibold leading-relaxed md:leading-relaxed text-white underline decoration-[#d4bf18] underline-offset-8 mt-36 text-4xl md:text-5xl"
       >
         BUILDING FUTURE
         <br />
         PATHWAYS
       </h1>
-      <p class="text-white text-sm md:text-base mx-8">
-        <span class="font-bold">Hawkstow</span>
-        <span class="">
-          is a privately owned multidisciplinary engineering enterprise with
-          employed dedicated professionals having extensive and proud
-          experiences in different construction services, assisted by
-          progressive and quixotic young people specializing in different fields
-          of the construction industry.
-        </span>
-      </p>
+
+      <div class="mx-24 md:mx-64 mt-48 justify-center">
+        <v-img
+          src="public/images/logo.webp"
+          max-width="60"
+          class="mx-auto"
+        />
+        <span class="text-[#34a90c] font-extrabold text-2xl">HAWK</span>
+        <span class="text-[#d4bf18] font-extrabold text-2xl">STOW</span>
+        <p class="text-white font-Lexend font-light">Construction and Development</p>
+      </div>
+
     </v-container>
-  </v-img>
 </template>
