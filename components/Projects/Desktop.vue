0e5bb991333e6f64e88351c607cb5851192a27ef
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-05 17:43:42 +0800

    implemented projects gallery page

--- components/Projects/Desktop.vue
+++ components/Projects/Desktop.vue
@@ -1,34 +1,42 @@
 <template>
-  <div class="h-100 mx-20 mt-4 lg:mt-4 xl:mt-16 text-center hidden-sm-and-down" align="center">
+  <div
+    class="h-100 mx-20 mt-4 lg:mt-4 xl:mt-16 text-center hidden-sm-and-down"
+    align="center"
+  >
     <div class="flex flex-row mb-16">
+      <NuxtLink to="/projects">
+        <v-img
+          src="assets/images/bg.png"
+          :width="200"
+          aspect-ratio="3/4"
+          lazy-src="assets/images/bg.png"
+          cover
+          class="mx-8"
+        />
+      </NuxtLink>
       <v-img
-      src="assets/images/bg.png"
-      :width="200"
-      aspect-ratio="3/4"
-      lazy-src="assets/images/bg.png"
-      cover
-      class="mx-8"
-    />
-    <v-img
-      src="assets/images/bg.png"
-      :width="200"
-      aspect-ratio="3/4"
-      lazy-src="assets/images/bg.png"
-      cover
-      class="mx-8"
-    />
-    <v-img
-      src="assets/images/bg.png"
-      :width="200"
-      aspect-ratio="3/4"
-      lazy-src="assets/images/bg.png"
-      cover
-      class="mx-8"
-    />
+        src="assets/images/bg.png"
+        :width="200"
+        aspect-ratio="3/4"
+        lazy-src="assets/images/bg.png"
+        cover
+        class="mx-8"
+      />
+      <v-img
+        src="assets/images/bg.png"
+        :width="200"
+        aspect-ratio="3/4"
+        lazy-src="assets/images/bg.png"
+        cover
+        class="mx-8"
+      />
     </div>
 
     <MarqueeDesktop :clients="clients" />
-    <p class="font-Lexend font-semibold text-[#175500] text-xl">&#38; <br /> PRIVATE BUSINESSES</p>
+    <p class="font-Lexend font-semibold text-[#175500] text-xl">
+      &#38; <br />
+      PRIVATE BUSINESSES
+    </p>
   </div>
 </template>
 
@@ -37,5 +45,3 @@ import { projectsData } from "@/data/projectsData";
 
 const clients = ref(projectsData);
 </script>
-
-
---
+++ pages/projects.vue
@@ -0,0 +1,15 @@
+<template>
+  <NavBarProjects />
+  <v-divider
+    class="border-opacity-100 border-[#175500]"
+    :thickness="4"
+  ></v-divider>
+
+  <div class="h-100 w-9/12 md:w-10/12 mx-auto mt-7 lg:mt-8 xl:mt-12 border">
+    <HeadingTitle :title="title" />
+  </div>
+</template>
+
+<script setup>
+const title = "Projects";
+</script>
