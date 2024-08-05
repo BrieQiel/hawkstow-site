6886818656b0e0794d84267d300a578def8f23ca
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-05 19:07:47 +0800

    divided projects page to each project

---
+++ pages/projects/government-buildings.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavBarProjects />
+  <v-divider
+    class="border-opacity-100 border-[#175500]"
+    :thickness="4"
+  ></v-divider>
+
+  <div class="h-100 w-9/12 md:w-10/12 mx-auto mt-2 lg:mt-8 xl:mt-12 ">
+    <HeadingTitle :title="title" />
+
+    govt buildings yeaaaa
+  </div>
+</template>
+
+<script setup>
+const title = "Projects";
+</script>
--- pages/projects/roads.vue
+++ pages/projects/roads.vue
@@ -5,8 +5,9 @@
     :thickness="4"
   ></v-divider>
 
-  <div class="h-100 w-9/12 md:w-10/12 mx-auto mt-7 lg:mt-8 xl:mt-12 border">
+  <div class="h-100 w-9/12 md:w-10/12 mx-auto mt-2 lg:mt-8 xl:mt-12 ">
     <HeadingTitle :title="title" />
+    roads yeaaaa
   </div>
 </template>
 
