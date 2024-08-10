e6b3fe4c2dbf435ecc5aea4c7ab85af89d986cc6
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-10 18:29:03 +0800

    used script instead

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,24 @@
 <template>
   <v-app>
-    <!-- HTML Meta Tags -->
-    <title>Hawkstow Construction and Development</title>
-    <meta name="description" content="Building Future Pathways" />
-
-    <!-- Facebook Meta Tags -->
-    <meta
-      property="og:url"
-      content="https://hawkstow-website-b83ac9bf6350.herokuapp.com/"
-    />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Hawkstow Construction and Development" />
-    <meta property="og:description" content="Building Future Pathways" />
-    <meta
-      property="og:image"
-      content="https://drive.google.com/file/d/18_T0GitkKjsCRRxigyym8741rUdVXV3n/view?usp=sharing"
-    />
-
-    <!-- Twitter Meta Tags -->
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta
-      property="twitter:domain"
-      content="hawkstow-website-b83ac9bf6350.herokuapp.com"
-    />
-    <meta
-      property="twitter:url"
-      content="https://hawkstow-website-b83ac9bf6350.herokuapp.com/"
-    />
-    <meta
-      name="twitter:title"
-      content="Hawkstow Construction and Development"
-    />
-    <meta name="twitter:description" content="Building Future Pathways" />
-    <meta
-      name="twitter:image"
-      content="https://drive.google.com/file/d/18_T0GitkKjsCRRxigyym8741rUdVXV3n/view?usp=sharing"
-    />
-
-    <!-- Meta Tags Generated via https://www.opengraph.xyz -->
-
     <slot />
   </v-app>
 </template>
 
-<script setup></script>
+<script setup>
+useHead({
+  meta: [
+    { name: 'description', content: 'Building Future Pathways' },
+    { property: 'og:url', content: 'https://hawkstow-website-b83ac9bf6350.herokuapp.com/' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:title', content: 'Hawkstow Construction and Development' },
+    { property: 'og:description', content: 'Building Future Pathways' },
+    { property: 'og:image', content: 'https://drive.google.com/file/d/18_T0GitkKjsCRRxigyym8741rUdVXV3n/view?usp=sharing' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { property: 'twitter:domain', content: 'hawkstow-website-b83ac9bf6350.herokuapp.com' },
+    { property: 'twitter:url', content: 'https://hawkstow-website-b83ac9bf6350.herokuapp.com/' },
+    { name: 'twitter:title', content: 'Hawkstow Construction and Development' },
+    { name: 'twitter:description', content: 'Building Future Pathways' },
+    { name: 'twitter:image', content: 'https://drive.google.com/file/d/18_T0GitkKjsCRRxigyym8741rUdVXV3n/view?usp=sharing' },
+  ]
+})
+</script>
