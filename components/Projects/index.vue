54856116ae6a49bdaabf29822a46ebca1d0e7588
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-10 15:47:37 +0800

    changed title

--- components/Projects/index.vue
+++ components/Projects/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-9/12 md:w-10/12 mx-auto mt-8 lg:mt-8 xl:mt-12">
-    <HeadingTitle title="Projects" />
+    <HeadingTitle title="Projects and Clients" />
 
     <ProjectsDesktop />
     <ProjectsMobile />
