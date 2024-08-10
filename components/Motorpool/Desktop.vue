2b43f281b9c3862fcdf7a5080f19600e9fbcf8a2
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-10 16:52:41 +0800

    motorpool WIP

--- components/Motorpool/Desktop.vue
+++ components/Motorpool/Desktop.vue
@@ -3,41 +3,7 @@
     class="h-100 mx-20 mt-8 lg:mt-12 xl:mt-20 text-center hidden-sm-and-down"
     align="center"
   >
-    <div class="flex flex-row md:mb-16 justify-center">
-      <div v-for="(project, index) in projects" :key="index">
-        <NuxtLink :to="`/projects/${project.link}`">
-          <v-img
-            :src="`/images/${project.thumbnail}`"
-            aspect-ratio="3/4"
-            :lazy-src="`/images/${project.thumbnail}`"
-            cover
-            class="mx-8 w-44 md:w-44 lg:w-52 xl:w-64 2xl:w-80"
-          >
-            <v-container
-              class="h-full font-Lexend text-lg text-white bg-[#84bf6e] opacity-5 hover:opacity-100 content-center"
-            >
-              <p class="tracking-wider">{{ project.title }}</p>
-            </v-container>
-          </v-img>
-        </NuxtLink>
-      </div>
-    </div>
-
-    <Marquee :fade="true" :pauseOnHover="true">
-      <div v-for="client in clients" class="w-40 mr-4 align-center">
-        <v-img
-          :src="`/images/clients/${client.logo}`"
-          :width="60"
-          aspect-ratio="1"
-          class="mx-auto mb-2"
-        />
-        <p class="text-center text-xs">{{ client.name }}</p>
-      </div>
-    </Marquee>
-    <p class="font-Lexend font-semibold text-[#175500] text-xl">
-      &#38; <br />
-      PRIVATE BUSINESSES
-    </p>
+    Motorpool
   </div>
 </template>
 
@@ -48,3 +14,4 @@ import { projectData } from "@/data/projectsData";
 const clients = ref(clientsData);
 const projects = ref(projectData);
 </script>
+
