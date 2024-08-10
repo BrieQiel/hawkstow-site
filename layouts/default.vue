08e3a6a4560aaabbe3386024ee7d98aaa14f8b44
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-10 18:41:00 +0800

    changed image link to proper viewing

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,13 +12,13 @@ useHead({
     { property: 'og:type', content: 'website' },
     { property: 'og:title', content: 'Hawkstow Construction and Development' },
     { property: 'og:description', content: 'Building Future Pathways' },
-    { property: 'og:image', content: 'https://drive.google.com/file/d/18_T0GitkKjsCRRxigyym8741rUdVXV3n/view?usp=sharing' },
+    { property: 'og:image', content: 'https://drive.google.com/uc?id=18_T0GitkKjsCRRxigyym8741rUdVXV3n' },
     { name: 'twitter:card', content: 'summary_large_image' },
     { property: 'twitter:domain', content: 'hawkstow-website-b83ac9bf6350.herokuapp.com' },
     { property: 'twitter:url', content: 'https://hawkstow-website-b83ac9bf6350.herokuapp.com/' },
     { name: 'twitter:title', content: 'Hawkstow Construction and Development' },
     { name: 'twitter:description', content: 'Building Future Pathways' },
-    { name: 'twitter:image', content: 'https://drive.google.com/file/d/18_T0GitkKjsCRRxigyym8741rUdVXV3n/view?usp=sharing' },
+    { name: 'twitter:image', content: 'https://drive.google.com/uc?id=18_T0GitkKjsCRRxigyym8741rUdVXV3n' },
   ]
 })
 </script>
