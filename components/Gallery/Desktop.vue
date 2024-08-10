9ce0e9e6a6d4507d78925a63ba6f990eda5965a0
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-10 16:53:06 +0800

    divided gallery into mobile and desktop view

---
+++ components/Gallery/Desktop.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="h-100 w-9/12 md:w-10/12 mx-auto mt-2 lg:mt-8 xl:mt-12 hidden-sm-and-down"
+  >
+    <HeadingTitle title="Gallery" />
+    <div class="mt-4 mb-8 flex flex-row align-end">
+      <div v-for="(project, index) in projects" :key="index">
+        <NuxtLink :to="`/gallery/${project.link}`">
+          <h1
+            class="font-Lexend font-semibold text-[#175500] mb-2 mx-8 cursor-pointer"
+            :class="isSelected(index)"
+          >
+            {{ project.title }}
+          </h1>
+        </NuxtLink>
+      </div>
+    </div>
+
+    <v-row class="justify-center">
+      <v-cols v-for="(image, index) in images" :key="index">
+        <v-img
+          :src="`/images/${image}`"
+          width="200"
+          :aspect-ratio="4 / 3"
+          cover
+          class="m-1 cursor-pointer"
+        >
+          <v-dialog activator="parent" class="w-1/2">
+            <v-img :src="`/images/${image}`" class="m-1"></v-img>
+          </v-dialog>
+        </v-img>
+      </v-cols>
+    </v-row>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    projects: Array,
+    images: Array,
+    isSelected: Function,
+});
+</script>
--- pages/gallery/[project].vue
+++ pages/gallery/[project].vue
@@ -1,30 +1,17 @@
 <template>
   <ProjectsNavbar />
-  <div class="h-100 w-9/12 md:w-10/12 mx-auto mt-2 lg:mt-8 xl:mt-12">
-    <HeadingTitle title="Gallery" />
-    <div class="mt-4 mb-8 flex flex-row align-end">
-      <div v-for="(project, index) in projects" :key="index">
-        <NuxtLink :to="`/projects/${project.link}`">
-          <h1
-            class="font-Lexend font-semibold text-[#175500] mb-2 mx-8"
-            :class="isSelected(index)"
-          >
-            {{ project.title }}
-          </h1>
-        </NuxtLink>
-      </div>
-    </div>
 
-    <v-row class="justify-center">
-        <v-cols v-for="(image, index) in images" :key="index">
-            <v-img :src="`/images/${image}`" width="200" :aspect-ratio="4/3" cover class="m-1 cursor-pointer">
-                <v-dialog activator="parent" max-width="1000" class="w-1/2">
-                    <v-img :src="`/images/${image}`" width="1000" class="m-1"></v-img>
-                </v-dialog>
-            </v-img>
-        </v-cols>
-    </v-row>
-  </div>
+  <GalleryDesktop
+    :projects="projects"
+    :images="images"
+    :isSelected="isSelected"
+  />
+
+  <GalleryMobile
+    :projects="projects"
+    :images="images"
+    :isSelected="isSelected"
+  />
 </template>
 
 <script setup>
@@ -46,10 +33,10 @@ onMounted(() => {
 });
 
 const isSelected = (index) => {
-  if (route.path === `/projects/${projects.value[index].link}`) {
+  if (route.path === `/gallery/${projects.value[index].link}`) {
     return "text-lg md:text-xl mb-2 underline";
   } else {
-    return "text-sm md:text-base mb-2 hover:underline hover:cursor-pointer";
+    return "text-sm md:text-base mb-2 hover:underline";
   }
 };
 </script>
