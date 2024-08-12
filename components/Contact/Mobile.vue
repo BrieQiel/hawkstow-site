b02200c399dfb7e62a3a32be2220d1affcdd9373
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-13 03:15:52 +0800

    implemented mobile and dedesktop views

---
+++ components/Contact/Mobile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mt-4 hidden-md-and-up">
+      <v-carousel
+      hide-delimiters
+      :show-arrows="false"
+      progress="#175500"
+      class="h-100">
+        <v-carousel-item>
+            <ContactForm />  
+        </v-carousel-item>
+        <v-carousel-item>
+            <ContactDetails />  
+        </v-carousel-item>
+      </v-carousel>
+    </div>
+</template>
