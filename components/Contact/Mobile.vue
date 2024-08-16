136b86ca66224febe9dfb5774eb7c61d4052986d
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-16 21:06:06 +0800

    replaced carousel with dialogs

--- components/Contact/Mobile.vue
+++ components/Contact/Mobile.vue
@@ -1,6 +1,21 @@
 <template>
-    <div class="mt-4 hidden-md-and-up">
-      <v-carousel
+  <div class="mt-4 hidden-md-and-up">
+    <div align="center" class="mb-2">
+      <v-btn density="comfortable" class="ping" color="#175500" text="">
+        Details
+        <v-dialog activator="parent" class="">
+          <div class="bg-white p-6 rounded-md">
+            <ContactDetails />
+          </div>
+        </v-dialog>
+      </v-btn>
+    </div>
+    <ContactForm />
+  </div>
+</template>
+
+<!--<span class="font-bold hidden-md-and-up">Swipe to view details <v-icon>mdi-arrow-right</v-icon>-->
+<!--<v-carousel
       hide-delimiters
       :show-arrows="false"
       progress="#175500"
@@ -11,6 +26,4 @@
         <v-carousel-item>
             <ContactDetails />  
         </v-carousel-item>
-      </v-carousel>
-    </div>
-</template>
+      </v-carousel>-->
