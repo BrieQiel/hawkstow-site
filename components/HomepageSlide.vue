89f4959f5f3f96f85e0df36ade06f2ea50e88ffe
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-16 21:04:41 +0800

    changed logo position and font color

--- components/HomepageSlide.vue
+++ components/HomepageSlide.vue
@@ -8,13 +8,13 @@
         PATHWAYS
       </h1>
 
-      <div class="justify-center absolute bottom-32 lg:bottom-32 xl:bottom-52 left-1/2 -translate-x-1/2">
+      <div class="justify-center absolute bottom-44 lg:bottom-32 xl:bottom-52 left-1/2 -translate-x-1/2">
         <v-img
           src="assets/images/logo.webp"
-          max-width="60"
+          max-width="60"  
           class="mx-auto"
         />
-        <span class="text-[#34a90c] font-extrabold text-xl lg:text-xl xl:text-2xl">HAWK</span>
+        <span class="text-[#428f28] font-extrabold text-xl lg:text-xl xl:text-2xl">HAWK</span>
         <span class="text-[#d4bf18] font-extrabold text-xl lg:text-xl xl:text-2xl">STOW</span>
         <p class="text-white font-Lexend font-light text-sm lg:text-sm xl:text-base">Construction and Development</p>
       </div>
