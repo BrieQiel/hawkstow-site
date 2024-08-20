b98ad0c47a3df9b565b683d7febe7d7334d1a858
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-21 01:16:33 +0800

    implemented motorpool slide

--- components/Motorpool/Desktop.vue
+++ components/Motorpool/Desktop.vue
@@ -1,17 +1,26 @@
 <template>
   <div
-    class="h-100 mx-20 mt-8 lg:mt-12 xl:mt-20 text-center hidden-sm-and-down"
+    class="h-100 w-10/12 mx-auto mt-8 lg:mt-12 xl:mt-20 hidden-sm-and-down"
     align="center"
   >
-    Motorpool
+    <v-row class="w-full">
+      <v-col v-for="feature in features" class="w-72 aspect-[3/4] text-left">
+        <v-img
+          :src="`/images/${feature.image}`"
+          class="mb-4"
+          aspect-ratio="0.75"
+          cover
+        />
+        <p class="italic text-sm lg:text-sm xl:text-base">
+          "{{ feature.quote }}"
+        </p>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script setup>
-import { clientsData } from "@/data/projectsData";
-import { projectData } from "@/data/projectsData";
+import { motorpoolData } from "@/data/servicesdata";
 
-const clients = ref(clientsData);
-const projects = ref(projectData);
+const features = ref(motorpoolData);
 </script>
-
--- components/Motorpool/Mobile.vue
+++ components/Motorpool/Mobile.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="h-100 text-center hidden-md-and-up " align="center">
-    Motorpool
+  <div class="h-100 text-left hidden-md-and-up mt-4" align="center">
+    <v-carousel
+      hide-delimiters
+      :show-arrows="false"
+      continuous
+      cycle
+      interval="3000"
+      progress="#175500"
+    >
+      <v-carousel-item v-for="feature in features" :key="feature.id">
+        <v-img
+          :src="`/images/${feature.image}`"
+          class="mb-4"
+          aspect-ratio="0.9"
+          cover
+        />
+        <p class="italic text-sm lg:text-sm xl:text-base">
+          "{{ feature.quote }}"
+        </p>
+      </v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
 <script setup>
-import { clientsData } from "@/data/projectsData";
-import { projectData } from "@/data/projectsData";
+import { motorpoolData } from "@/data/servicesdata";
 
-const clients = ref(clientsData);
-const projects = ref(projectData);
+const features = ref(motorpoolData);
 </script>
-
-
