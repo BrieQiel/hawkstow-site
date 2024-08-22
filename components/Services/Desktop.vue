4df47be75112d44059e84706d41fd1ad0d4949c6
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-08-22 17:54:30 +0800

    added video feature

--- components/Services/Desktop.vue
+++ components/Services/Desktop.vue
@@ -3,12 +3,17 @@
     class="h-100 mx-20 mt-12 lg:mt-12 xl:mt-20 flex flex-row text-left hidden-sm-and-down"
     align="center"
   >
+    <video v-if="video" controls autoplay muted disablepictureinpicture loop width="350">
+      <source :src="`/videos/${video}`" type="video/mp4" />
+    </video>
     <v-img
-      :src="`/images/${image}`"
+      v-else
+      :src="`/images/services/${image}`"
       :width="350"
-      aspect-ratio="3/4"
-      :lazy-src="`/images/${image}`"
+      aspect-ratio="1.67"
+      :lazy-src="`/images/services/${image}`"
       cover
+      class="border"
     />
     <div class="mx-10">
       <p class="text-sm lg:text-sm xl:text-base">{{ description }}</p>
@@ -20,5 +25,6 @@
 const props = defineProps({
   description: String,
   image: String,
+  video: String,
 });
 </script>
