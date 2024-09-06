f5491f3c1a94bc3433172797aadb3b8b7108fa35
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-09-06 12:32:02 +0800

    changed title

--- components/Motorpool/index.vue
+++ components/Motorpool/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-9/12 md:w-10/12 mx-auto mt-8 lg:mt-8 xl:mt-12">
-    <HeadingTitle title="Motorpool" />
+    <HeadingTitle title="Hawkstow Construction - Motorpool and Compound" />
 
     <MotorpoolDesktop />
     <MotorpoolMobile />
