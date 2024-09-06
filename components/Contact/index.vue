90bdf81e34019c678ee66a900291e02fbc32d5b6
Bianca Marie Ligan <94683008+BrieQiel@users.noreply.github.com>
2024-09-06 12:30:51 +0800

    changed title to "Contact Us"

--- components/Contact/index.vue
+++ components/Contact/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-9/12 md:w-10/12 mx-auto mt-8 lg:mt-8 xl:mt-12">
-    <HeadingTitle title="Contact" />
+    <HeadingTitle title="Contact Us" />
 
     <ContactDesktop />
     <contactMobile />
